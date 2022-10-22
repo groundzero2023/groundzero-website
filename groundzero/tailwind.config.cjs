a96880e5f5e38ff75ee71b31fc6d540201425ab9
florentianayuwono <e0851439@u.nus.edu>
2022-10-22 20:28:37 +0800

    Configure project theme styling

--- groundzero/tailwind.config.cjs
+++ groundzero/tailwind.config.cjs
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryBackground: "#1F1B40",
+        secondaryBackground: "#2B2853",
+        foregroundOrange: "#F28F48",
+        foregroundBlue: "#69A9CE",
+        foregroundViolet: "#A733BB",
+        dimWhite: "rgba(255, 255, 255, 0.7)",
+        dimBlue: "rgba(9, 151, 124, 0.1)",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        lexendDeca: ["Lexend Deca", "Plus Jakarta Sans"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
   },
   plugins: [],
-}
+};
