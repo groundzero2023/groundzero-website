795a9769fcbb0813bc5f6497180bd43c6f9ffaa1
Jason Christopher <jason.christopher@u.nus.edu>
2022-12-03 01:28:52 +0800

    Add Layout and UI Component
    Add Layout and UI component to naviagation bar skeleton.
    
    Modifications:
    - Add classname for Navigation Bar as nav
    - Add Poppins font to `index.html` through google fonts import
    - Responsive page design for desktop. On progress for mobile and smaller pages.

--- groundzero/src/components/NavigationBar.jsx
+++ groundzero/src/components/NavigationBar.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import "../index.css"
 
 const NavigationBar = () => {
   return (
-    <div>NavigationBar</div>
+    <nav className="nav">
+      <a href="/" className="site-title">[image]</a>
+      <ul>
+        <li>
+          <a href="/About">About</a>
+        </li>
+        <li>
+          <a href="/Timeline">Timeline</a>
+        </li>
+        <li>
+          <a href="/Problems">Problems</a>
+        </li>
+        <li>
+          <a href="/Contact">Contact</a>
+        </li>
+        <li id="nav-register-button">
+          <a href="/Register">Register</a>
+        </li>
+      </ul> 
+    </nav>
   )
 }
 
