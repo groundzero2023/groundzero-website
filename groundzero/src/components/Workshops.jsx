efae877c4369b73697a1b74a9197771f9697050e
t1mzzz <timothykoei@gmail.com>
2022-12-09 21:31:13 +0700

    Modify responsiveness in Workshop page

--- groundzero/src/components/Workshops.jsx
+++ groundzero/src/components/Workshops.jsx
@@ -3,22 +3,22 @@ import { workshops } from '../constants'
 
 const Workshops = () => {
   return (
-    <div className="grid place-items-center text-white px-40">
+    <div className="grid place-items-center text-white xs:px-16 lg:px-40">
       <div className="heading">
         Pre-Ground Zero Workshops
       </div>
       <div className="font-light text-2xl pb-12 text-justify">
         Lorem ipsum dolor sit amet. Qui nihil nesciunt et minus minus id veniam corporis et explicabo aperiam sed quia dolore aut quod soluta.
       </div>
-      <div className="grid grid-cols-2 gap-x-6 gap-y-10">
+      <div className="grid sm:grid-cols-2 gap-x-6 gap-y-10">
         {workshops.map((workshop) => (
-          <div className="cursor-pointer rounded-xl bg-[#2B2853] opacity-60 transition w-auto h-44 p-4 relative hover:scale-105 hover:opacity-100">
-            <div className="h-1/2 items-start grid place-items-center text-center underline font-semibold text-xl">
+          <div className="cursor-pointer rounded-xl bg-[#2B2853] lg:opacity-60 transition w-auto h-44 p-4 relative lg:hover:scale-105 lg:hover:opacity-100">
+            <div className="h-2/5 items-start grid place-items-center text-center underline font-semibold text-xl sm:text-base md:text-xl">
               <p>
                 {workshop.name}
               </p>
             </div>
-            <div className="h-1/2 text-center font-light grid place-items-center">
+            <div className="h-3/5 sm:text-sm md:text-base text-center font-light grid place-items-center">
               <div className="h-max flex items-end">
                 <h3 className="whitespace-pre-line">
                   {
