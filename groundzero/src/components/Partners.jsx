a72dd58b915df9d979331021ddf664b56fe6faa8
florentianayuwono <e0851439@u.nus.edu>
2022-12-15 23:21:05 +0800

    Make Partners centered

--- groundzero/src/components/Partners.jsx
+++ groundzero/src/components/Partners.jsx
@@ -8,7 +8,7 @@ const Partners = () => (
     id="partners"
     className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}
   >
-    <div className="w-full justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
+    <div className="w-full text-center justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
       <h1 className={styles.heading2}>Partners</h1>
     </div>
     <div className={`${styles.flexCenter} flex-wrap w-full`}></div>
