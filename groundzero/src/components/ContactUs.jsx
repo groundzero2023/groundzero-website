593c1b1f828f1f7f83232dd98aa2c3361899fb73
florentianayuwono <e0851439@u.nus.edu>
2022-12-15 21:43:15 +0800

    Fix wrapping

--- groundzero/src/components/ContactUs.jsx
+++ groundzero/src/components/ContactUs.jsx
@@ -10,7 +10,7 @@ const ContactUs = () => (
   >
     <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient" />
 
-    <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
+    <div className="w-full justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
       <h1 className={styles.heading2}>Contact Us</h1>
       <div className="w-full md:mt-0 mt-6">
         <p className={`${styles.paragraph} text-left max-w-[450px]`}>
