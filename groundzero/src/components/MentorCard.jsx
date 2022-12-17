25a28a7b35590645b04b8fc9ad16267b7a81c4a1
t1mzzz <timothykoei@gmail.com>
2022-12-17 16:46:07 +0700

    Add keyboard shortcut when opening card

--- groundzero/src/components/MentorCard.jsx
+++ groundzero/src/components/MentorCard.jsx
@@ -1,18 +1,48 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import { mentors } from "../constants";
 
 const MentorCard = ({mentor, setOpenCard, setCurrMentor}) => {
+  
+  const openPrevCard = () => {
+    setCurrMentor(mentors[mentors.indexOf(mentor) - 1]);
+  }
+
+  const openNextCard = () => {
+    setCurrMentor(mentors[mentors.indexOf(mentor) + 1]);
+  }
+
   const closeMentorCard = () => { 
     setOpenCard(false);
 
     // Disables Background Scrolling whilst the Mentor Card is open
     document.body.style.overflow = 'unset';
   }
+
+  // handle what happens on key press
+  const handleKeyPress = (event) => {
+    if (event.key == 'ArrowRight' && mentors.indexOf(mentor) != mentors.length - 1) {
+      openNextCard();
+    }
+    else if (event.key == "ArrowLeft" && mentors.indexOf(mentor) != 0) {
+      openPrevCard();
+    }
+  };
+
+//   useEffect(() => {
+//     // attach the event listener
+    document.addEventListener('keydown', handleKeyPress);
+
+    // // remove the event listener
+    // return () => {
+    //   document.removeEventListener('keydown', handleKeyPress);
+    // };
+//   }, [handleKeyPress]);
+
   return (
     <div className="flex place-items-center items-center justify-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-gray-600/60 backdrop-blur">
       
       <div className={`${mentors.indexOf(mentor) == 0 ? 'hidden' : 'block'} absolute left-12 cursor-pointer mr-8 text-2xl px-4 py-2 bg-black rounded-full text-center grid place-items-center`}
-      onClick={() => setCurrMentor(mentors[mentors.indexOf(mentor) - 1])}>
+      onClick={() => openPrevCard()}>
         &lt;
       </div>
       
@@ -61,7 +91,7 @@ const MentorCard = ({mentor, setOpenCard, setCurrMentor}) => {
       </div>
 
       <div className={`${mentors.indexOf(mentor) == mentors.length - 1 ? 'hidden' : 'block'} absolute right-12 cursor-pointer ml-8 text-2xl px-4 py-2 bg-black rounded-full text-center grid place-items-center`}
-      onClick={() => setCurrMentor(mentors[mentors.indexOf(mentor) + 1])}>
+      onClick={() => openNextCard()}>
         &gt;
       </div>
       
