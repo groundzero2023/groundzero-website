45acf3be96426bd067d68b419e15a2b70af01811
Jason Christopher <jason.christopher@u.nus.edu>
2023-01-05 03:58:53 +0800

    Fix unused import in Homepage

--- groundzero/src/components/HomePage.jsx
+++ groundzero/src/components/HomePage.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import styles from "../style";
 import { groundzero, homebg } from '../assets';
-import { homePage } from '../constants'
 
 const HomePage = () => {
     return (
