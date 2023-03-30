bf0d20bd976c3003e36d4618e046cbd3e201a15d
t1mzzz <timothykoei@gmail.com>
2023-03-30 11:54:47 +0800

    Adjust sizing for mobile responsiveness

--- groundzero/src/components/Mentors.jsx
+++ groundzero/src/components/Mentors.jsx
@@ -3,12 +3,12 @@ import styles from '../style';
 
 const Mentors = () => {
   return (
-    <div className={`grid ${styles.paddingY} place-items-center text-white xs:px-16 ss:px-12 sm:px-16 lg:px-10`} id="mentors">
+    <div className={`grid ${styles.paddingY} place-items-center text-white px-5 ss:px-10 sm:px-16 lg:px-10`} id="mentors">
       <div className="heading">
         Mentors
       </div>
-      <div className={`flex flex-col space-y-8 px-16`}>
-        <h1 className="text-xl">
+      <div className={`flex flex-col space-y-8 sm:px-16`}>
+        <h1 className="text-xl sm:text-2xl">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at rutrum arcu, vitae fringilla magna. Proin bibendum massa non turpis pellentesque, sed congue enim volutpat. 
         </h1>
         <div className="grid place-items-center">
