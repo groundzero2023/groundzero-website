e7e5fa8628cb64c135b517b501332d97633e6e66
t1mzzz <timothykoei@gmail.com>
2023-03-30 11:22:53 +0800

    Modify padding for `MentorPage`

--- groundzero/src/components/MentorGrid.jsx
+++ groundzero/src/components/MentorGrid.jsx
@@ -17,7 +17,7 @@ const MentorGrid = () => {
     }
 
     return (
-        <div className={`bg-primary ${styles.paddingY} ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`bg-primary ${styles.paddingY} xs:px-16 ss:px-12 sm:px-16 lg:px-12`}>
             <div className={`${styles.boxWidth}`}>
                 <div className="grid gap-8 ss:grid-cols-2 ss:gap-1 sm:grid-cols-2 sm:gap-2 md:grid-cols-3 lg:grid-cols-4 lg:gap-1">
                 {mentors.map((mentor) => (
@@ -37,13 +37,13 @@ const MentorGrid = () => {
                     <div className="w-full h-full opacity-0 hover:opacity-100 z-10 transition ease-in-out text-white">
                         <div className="bg-gradient-to-t w-full from-black absolute text-left bottom-0 left-0 p-4 pt-[10.5rem] object-fit">
                             <h2 className="text-xl font-bold">
-                            {mentor.title}
+                                {mentor.title}
                             </h2>
                             <h2 className="font-semibold">
-                            {mentor.subtitle}
+                                {mentor.subtitle}
                             </h2>
                             <h2 className="text-sm font-light">
-                            {mentor.info}
+                                {mentor.info}
                             </h2>
                         </div>
                     </div>
@@ -51,8 +51,8 @@ const MentorGrid = () => {
                 ))}
             </div>
             {
-            openCard && 
-            <MentorCard mentor={currMentor} setOpenCard={setOpenCard} setCurrMentor={setCurrMentor}/>
+                openCard && 
+                <MentorCard mentor={currMentor} setOpenCard={setOpenCard} setCurrMentor={setCurrMentor}/>
             }
             </div>
         </div>
