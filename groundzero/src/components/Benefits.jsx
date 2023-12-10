5d6dc072e848fd2bb76a219ceb869a2da0a1f887
wjacobw <williamjacob0910@gmail.com>
2023-12-11 00:46:41 +0800

    add website skeleton

---
+++ groundzero/src/components/Benefits.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavigationBar from "./NavigationBar";
+
+function Benefits() {
+  return (
+    <div>
+      <NavigationBar />
+      Benefits
+    </div>
+  );
+}
+
+export default Benefits;
---
+++ groundzero/src/components/Eligibility.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavigationBar from "./NavigationBar";
+
+function Eligibility() {
+  return (
+    <div>
+      <NavigationBar />
+      Eligibility
+    </div>
+  );
+}
+
+export default Eligibility;
