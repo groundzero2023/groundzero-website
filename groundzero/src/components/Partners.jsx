891c4e1891a1301614d7385d06746aa15fd8e623
jamesebond <e1032444@u.nus.edu>
2024-05-13 16:49:23 +0800

    phone update fomopay

--- groundzero/src/components/Partners.jsx
+++ groundzero/src/components/Partners.jsx
@@ -187,16 +187,9 @@ Its initiatives and global connections support a range of entrepreneurial journe
                 <div className="sponsor-description">
                   <br></br>
                   <br></br>
-                    <h1>About FOMO Pay:</h1>
-                    <p>
-                    Founded in 2015, FOMO Pay Pte Ltd is a major payment institution (License No. PS20200145) regulated under the Payment Services Act in Singapore, licensed by the Monetary Authority of Singapore (MAS) to conduct Cross-border Money Transfer Service, Domestic Money Transfer Service, Digital Payment Token Service, and Merchant Acquisition Service. The firm has become a leading one-stop digital payment, digital banking, and digital asset solution provider. It is currently building Asia’s fully licensed financial platform, helping institutions and businesses connect between fiat and digital currency. The firm offers its three flagship products:
+<p>
+<FOMOPayDetails></FOMOPayDetails>           
 
-FOMO Payment – One-stop digital payment solution for merchants, corporates and financial institutions
-FOMO iBank – Facilitate businesses’ everyday requirements for transactional banking needs
-FOMO Treasury – One-stop digital asset services provider bridging Web 2.0 & Web 3.0
-
-Visit www.fomopay.com for more information.
-                    
                     </p>
                 </div>
                 </div>
